fcadeeafa2308f750585fb1fb4efd474b93b088c
Jake Butler <tjakebutler@gmail.com>
2021-04-11 11:42:14 -0500

    Created a Checkout page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import Header from './Header'
-import Home from './Home'
-import './App.css';
+import Header from "./Header";
+import Checkout from "./Checkout";
+import Home from "./Home";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     //BEM convention
-    <div className="app">
-      
-      <Header/>
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Header />
+        <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -2,14 +2,18 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
+      <Link to="/">
       <img
         className="header__logo"
         src="https://mikekitko.com/wp-content/uploads/2019/10/amazon-logo-white-768x232.png"
       />
+      </Link>
+      
       <div className="header__search">
         <input className="header__searchInput" type="text"></input>
         <SearchIcon className="header__searchIcon" />
@@ -30,11 +34,15 @@ function Header() {
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
         </div>
-
+        
+        <Link to="/checkout">
         <div className="header__optionBasket">
             <ShoppingBasketIcon/>
             <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
+        
+        </Link>
+        
       </div>
     </div>
   );
