e703ca60d90d0523faa8b28bd1ed4600ddb31c59
Jake Butler <tjakebutler@gmail.com>
2021-04-12 20:42:13 -0500

    checkout page is complete, shopping basket now is able to display all item inside it and each item has a remove item from basket button which removes item onClick

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Checkout.css'
+import CheckoutProduct from './CheckoutProduct';
+import { useStateValue } from './StateProvider';
 import Subtotal  from "./Subtotal";
 
 
 
 
 function Checkout() {
+    const [{basket}, dispatch] = useStateValue()
     return (
         <div className='checkout'>
             <div className="checkout__left">
@@ -18,11 +21,18 @@ function Checkout() {
                     <h2 className="checkout__title">
                         Your Shopping Basket
                     </h2>
-                    {/* Basket Item */}
-                    {/* Basket Item */}
-                    {/* Basket Item */}
-                    {/* Basket Item */}
-                    {/* Basket Item */}
+
+                    {basket.map(item =>
+                        <CheckoutProduct
+                            id={item.id}
+                            title={item.title}
+                            image={item.image}
+                            price={item.price}
+                            rating={item.rating}
+                        />
+                        )}
+                    
+                    
                 </div>
             </div>
 
