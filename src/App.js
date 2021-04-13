5c05c3436d3bfb04661f6ba18f7512d58395aacc
Jake Butler <tjakebutler@gmail.com>
2021-04-13 08:42:33 -0500

    styled the login page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./Header";
 import Checkout from "./Checkout";
 import Home from "./Home";
+import Login from "./Login";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -11,12 +12,20 @@ function App() {
       <div className="app">
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Header />
+        
         <Switch>
+
+        <Route path="/login">
+            <Login/>
+          </Route>
+
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
+
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
